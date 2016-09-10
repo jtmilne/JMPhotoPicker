183c5dff007de063310f25baa6314072a3f73248
Joel Milne <joel.milne@gmail.com>
2016-09-10 08:05:26 -0700

    Fix git ignore for Xcode project. Add static library project.

---
+++ JMPhotoPickerLib/JMPhotoPickerLib/JMPhotoPickerLib.m
@@ -0,0 +1,13 @@
+//
+//  JMPhotoPickerLib.m
+//  JMPhotoPickerLib
+//
+//  Created by Joel Milne on 9/9/16.
+//  Copyright © 2016 Emotely. All rights reserved.
+//
+
+#import "JMPhotoPickerLib.h"
+
+//@implementation JMPhotoPickerLib
+
+//@end
